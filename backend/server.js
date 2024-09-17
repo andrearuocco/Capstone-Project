e17157b91cbf381077fe4c3dcdc735763eb97ca5
andrearuocco <ruoca96@gmail.com>
2024-09-17 09:33:38 +0200

    start capstone initialization

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+/* import passport from 'passport'
+import GoogleStrategy from './config/passport.config.js' 
+import authorization from './middleware/authorization.js' */
+
+const port = process.env.PORT || 5000
+const host = process.env.HOST || 'http://localhost'
+const server = express()
+
+server.use(express.json()) // express è un middleware utilizzato in modo che il server riconosca come JSON il body delle richieste
+server.use(cors()) // cors è un middleware che consente la connessione tra il server di backend e quello di frontend
+server.use(morgan("dev")) // middleware che mostra i log delle richieste http
+server.use(helmet()) // modulo che aiuta a proteggere le applicazioni
+
+await mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connessione al database...'))
+    .catch((err) => console.log(err))
+
+server.listen(port, () => {
+    console.log(`Capstone's server is listening on ${host}:${port}`)
+})
