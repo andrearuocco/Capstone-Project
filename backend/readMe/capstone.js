24b69c4d683dc56b38fe2f58f630cf320496db49
andrearuocco <ruoca96@gmail.com>
2024-09-24 14:00:31 +0200

    to start front fetch

--- backend/readMe/capstone.js
+++ backend/readMe/capstone.js
@@ -1,12 +1,12 @@
 // PER CONCLUSIONE BACKEND
 
-    // inserimento dell' autenticazione non solo per il login ma anche per distinguere admin da employee (lato front-end questa cosa come potrebbe essere gestita: 
-    // ?? con una pagina per la registrazione adimin accessibile con delle credenziali fornite da chi ha la licenza dell'applicazione ?? _ 26/09/2024)
+    // inserimento dell' AUTENTICAZIONE non solo per il login ma anche per distinguere ADMIN da EMPLOYEE /* (lato front-end questa cosa come potrebbe essere gestita: 
+    // ?? con una PAGINA per la registrazione adimin accessibile con delle CREDENZIALI fornite da chi ha la licenza dell'applicazione ?? _ */ 26/09/2024 /* ) */
 
     // 23 SETT: controllare la corretta cancellazione dei commenti legati e dei task assegnati quando viene cancellata un'utenza profile o un documento employee =>
     // ?? probabilmente una volta cancellato uno tra employee e profile si cancellano automaticamente anche i task in quanto la cancellazione nel secondo caso è gestita per il doc. corrispondente ??
     // (i commenti e i pagamenti invece restano nel DB in quanto refenziati e dotati quindi di una propria collection);
-    // 26/09/2024 getdailytask
+    // 26/09/2024 /* getdailytask */
 
     // per quanto riguarda la richiesta di permessi (?? ferie ??), 
     // cosa fare lato back-end per disegnare una situazione dove in seguito alla richiesta generata da un employee ed approvata da un admin si possa aggiornare in automatico paid/unpaid Leave (35-44)
