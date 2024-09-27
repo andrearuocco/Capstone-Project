bfbbcc7322814aa5c8f32309c8700f6891f7c06e
andrearuocco <ruoca96@gmail.com>
2024-09-27 10:55:46 +0200

    list of profile

--- frontend/src/components/profile/ProfileOne.jsx
+++ frontend/src/components/profile/ProfileOne.jsx
@@ -4,7 +4,7 @@ import './ProfileOne.css'
 function ProfileOne({profile}) {
     console.log(profile) 
     return (<Col xs={12} sm={6} md={4} lg={3} className="my-3">
-        <Card style={{ width: '18rem' }} >
+        <Card /* style={{ width: '18rem' }} */ >
             <Card.Img variant="top" src={profile.avatar} style={{ height: '18rem' }} />
             <Card.Body>
                 <Card.Title>{profile.name} {profile.surname} {` - ${profile.whoIs.type}`}</Card.Title>
@@ -18,11 +18,18 @@ function ProfileOne({profile}) {
                     {profile.whoIs.type === 'employee' && (
                         <ul className='list-unstyled'>
                             <li>Role: {profile.whoIs.employeeData.role}</li>
-                            {profile.whoIs.employeeData.dailyTask.map(dT =>  <li className='overF'>dailyTask: {dT.description} {dT.day}</li> )}
+
+                            {profile.whoIs.employeeData.dailyTask.length > 0 && (
+                                <li className='overF'>
+                                    dailyTask: 
+                                    {profile.whoIs.employeeData.dailyTask[profile.whoIs.employeeData.dailyTask.length - 1].description} {/* mostro solo il task assegnato più di recente */}
+                                    {profile.whoIs.employeeData.dailyTask[profile.whoIs.employeeData.dailyTask.length - 1].day}
+                                </li>
+                            )}
                         </ul>
                     )}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">ProfileDetails</Button>
             </Card.Body>
         </Card>
     </Col>)
