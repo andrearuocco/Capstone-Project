3d94a04d354237d59551eb62d02ec20a15463aa4
andrearuocco <ruoca96@gmail.com>
2024-10-01 13:19:59 +0200

    addPayments not

--- frontend/src/components/payEnvelope/PayEnvelope.jsx
+++ frontend/src/components/payEnvelope/PayEnvelope.jsx
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 import { Row, Col, Container, Button, Form } from 'react-bootstrap/';
 
 function PayEnvelope() {
-
+    const { id } = useParams() // prendo id dalla richiesta 
+    /* const [prof, setProf] = useState({profile.find(p => p._id === id)}) */ // cerco il profilo corrispondente a quello della richiesta per mostrare i suoi dati
+    const [employee, setEmployee] = useState({}) // creo uno stato perché avrò bisogno del doc. employee corrisponde a quello del profile cercato
 
     return (
         <Container>
