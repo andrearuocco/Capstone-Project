534681290cc16d06333c96e1e98379b31f480f5d
andrearuocco <ruoca96@gmail.com>
2024-10-09 12:28:19 +0200

    offcanvas css

--- frontend/src/components/profile/ProfileOne.jsx
+++ frontend/src/components/profile/ProfileOne.jsx
@@ -45,45 +45,47 @@ function ProfileOne({ profile }) {
                         {offcanvasConfig.name}
                     </Button>
                     <Offcanvas show={show} onHide={handleClose} scroll={offcanvasConfig.scroll} backdrop={offcanvasConfig.backdrop} className='bord-canvas'>
-                        <Offcanvas.Header closeButton>
+                        <Offcanvas.Header closeButton className='bg-gradient bg-opacity-10 bg-dark text-white-50 text-b-po mb-4'>
                             <Offcanvas.Title>Profile Details</Offcanvas.Title>
                         </Offcanvas.Header>
                         <Offcanvas.Body>
-                            <ul className='list-unstyled'>
-                                <li className='d-flex justify-content-between align-items-center mb-4'>
-                                    <Card.Img variant="top" className='border-50 me-3' src={profile.avatar} />
-                                    <div>{profile.name} {profile.surname}</div>
+                            <ul className='list-unstyled d-flex flex-column'>
+                                <li className='d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center mb-4 pb-4'>
+                                    <Card.Img variant="top" className='border-50 me-3 mb-4 mb-sm-0' src={profile.avatar} />
+                                    <div className='text-black-50'>{profile.name} {profile.surname}</div>
                                     <div>{profile.whoIs.type === 'admin' && (
                                         <ul className='list-unstyled'>
-                                            <li>Role: {profile.whoIs.adminData.name}</li>
+                                            <li className='text-black-50'><span className='text-white text-opacity-50'>Role:</span> {profile.whoIs.adminData.name}</li>
                                         </ul>
                                     )}
                                     {profile.whoIs.type === 'employee' && (
                                         <ul className='list-unstyled'>
-                                            <li>Role: {profile.whoIs.employeeData.role}</li>
+                                            <li className='text-black-50'><span className='text-white text-opacity-50'>Role:</span> {profile.whoIs.employeeData.role}</li>
                                         </ul>
                                     )}</div>
                                 </li>
-                                <li className='mb-4'>Email: {profile.email}</li>
-                                <li className='mb-4'>Birthday: {new Date(profile.birthday).toLocaleDateString('it-IT')}</li>
-                                <li className='mb-4'>Country: {profile.country}</li>
-                                <li className='mb-4'>IBAN: {profile.IBAN}</li>
-                                <li className='mb-4'>TIN: {profile.TIN}</li>
+                                <li className='mb-4 typing-text mt-3 text-black-50'><span className='text-white text-opacity-50'>Email:</span> {profile.email}</li>
+                                <li className='mb-4 typing-text text-black-50'><span className='text-white text-opacity-50'>Birthday:</span> {new Date(profile.birthday).toLocaleDateString('it-IT')}</li>
+                                <li className='mb-4 typing-text text-black-50'><span className='text-white text-opacity-50'>Country:</span> {profile.country}</li>
+                                <li className='mb-4 typing-text text-black-50'><span className='text-white text-opacity-50'>IBAN:</span> {profile.IBAN}</li>
+                                <li className='mb-4 typing-text text-black-50'><span className='text-white text-opacity-50'>TIN:</span> {profile.TIN}</li>
                                 {profile.whoIs.type === 'employee' && (
-                                    <> {profile.whoIs.employeeData.dailyTask.map((dT, index) => <div>dailyTask{index}:{dT.description} 
-                                            <p><span className='me-2'>DAY:{dT.day}</span> 
-                                            WHEN:{dT.when}</p>
+                                    <> {profile.whoIs.employeeData.dailyTask.map((dT, index) => <div className='typing-text'><span className='text-white text-opacity-50'>dailyTask{index}:</span><p className='text-black-50 mb-0'>{dT.description}</p>
+                                            <p className='text-black-50 mt-0'><span className='me-2'><span className='text-white text-opacity-50'>DAY:</span>{dT.day}</span> 
+                                            <span className='text-white text-opacity-50'>WHEN:</span>{dT.when}</p>
                                         </div>)}
                                     </>
                                 )}
-                                <li><Button as={Link} to={`/${profile._id}`} variant="primary" className='mb-4'>Modifica Posizione Lavorativa</Button></li>
-                                {profile.whoIs.employeeData && profile.whoIs.employeeData._id && (
-                                    <Button as={Link} to={`/${profile._id}/payments/${profile.whoIs.employeeData._id}`} variant="primary" className='mb-4'>
+                             
+                                {profile.whoIs.employeeData && profile.whoIs.employeeData._id && (<>
+                                    <Button as={Link} to={`/${profile._id}`} className='mt-4 mb-4 button-blue-po w-50'>Posizione Lavorativa</Button>
+                                    <Button as={Link} to={`/${profile._id}/payments/${profile.whoIs.employeeData._id}`} className='mb-4 button-blue-po w-50'>
                                         Aggiungi Pagamento
-                                    </Button>
+                                    </Button></>
                                 )}
                             </ul>
                         </Offcanvas.Body>
+                        <footer className='offc-f sticky-bottom'>Amministrazione & Gestione</footer>
                     </Offcanvas>
                 </>
             </Card.Body>
